31ccabe8c52bf78eb4ba87557d6ea0780242603f
dvank1mang1 <dbogolaev@mail.ru>
2025-02-02 19:21:00 +0300

    Body for tg-Bot

---
+++ bot.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = '8009392899:AAEgbsEom6hAksj6QIsvQboqq8XBLknpq60'
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Handler for the /start command"""
+    await update.message.reply_text('Hello! I am your new Telegram bot!')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Echo the user message"""
+    await update.message.reply_text(update.message.text)
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    application.run_polling()
+
+if __name__ == '__main__':
+    main()
